ffadea75303f054c896a5976008f1b690d1c64b7
luis cabrera <l.cabrera21@gmail.com>
2024-09-01 11:38:50 -0400

    revision_02

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -19,11 +19,11 @@ const Carrito = () => {
                             <div key={prod.id}>
                                 <h2>{prod.titulo}</h2>
                                 <p>Precio unidad: {prod.precio} $</p>
-                                <p>Precio total: {prod.precio * prod.cantidad} €</p>
+                                <p>Precio total: {prod.precio * prod.cantidad} $</p>
                                 <p>Cant: {prod.cantidad}</p>
                             </div>
                         ))}
-                        <h2>Precio total: {precioTotal()} €</h2>
+                        <h2>Precio total: {precioTotal()} $</h2>
                         <div className="button-container">
                             <button onClick={handleVaciar} className="agregar-al-carrito">Vaciar</button>
                             <Link to="/checkout" className='checkout'>Finalizar compra</Link>
